6f06bbee50b539c0d411e6cdefba2c0b64083e64
owen.huang <firebushtree@163.com>
2024-01-25 16:37:59 +0800

    feat: support browser save document locally

--- src/utils/browser.ts
+++ src/utils/browser.ts
@@ -1,3 +1,29 @@
 import { type SwaggerDocument } from '.'
 
-export const saveDocumentLocally = (document: SwaggerDocument) => {}
+const DOCUMENT_LOCALLY_KEY = 'swagger-client-document-config'
+export const getDocumentLocally = () => {
+  const fileContent = localStorage.getItem(DOCUMENT_LOCALLY_KEY) ?? ''
+  try {
+    const documentList = JSON.parse(fileContent) as SwaggerDocument[]
+    return documentList
+  } catch (e) {
+    return []
+  }
+}
+export const saveDocumentLocally = (document: SwaggerDocument) => {
+  const documentList = getDocumentLocally()
+  if (documentList.length > 0) {
+    const targetIdx = documentList.findIndex(item => item.address === document.address)
+    if (targetIdx === -1) {
+      // create
+      documentList.push(document)
+    } else {
+      // update
+      documentList[targetIdx] = document
+    }
+
+    localStorage.setItem(DOCUMENT_LOCALLY_KEY, JSON.stringify(documentList))
+  } else {
+    localStorage.setItem(DOCUMENT_LOCALLY_KEY, JSON.stringify([document]))
+  }
+}
